0e5b361f1c68f12f161d3cba98a604c0cce293aa
nurwahidmuslim <nurwahidmuslim32@gmail.com>
2024-10-03 16:17:31 +0700

    tugas2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profile/{nama}/{kelas}/{npm}',
+[ProfileController::class, 'profile']);
+Route::get('/profile', [ProfileController::class, 'profile']);
